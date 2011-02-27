49ed4fca62cbe2b122a15a33739399c9f4643892
Quang Huy <huyatu@yahoo.com>
2011-02-27 21:52:49 +0800

    Personal Hello

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main (int argc, char **argv)
+#include <string.h>
+
+int main(int argc, char **argv)
 {
-	print("Hello world!\n");
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	printf("length = %d\n", strlen(name)); /* debug line */
+	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+	printf("Hello %s!\n", name);
 	return 0;
 }
